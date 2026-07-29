0de3e2134960dfc6caa6c387cf2b509d288fba6b
princekumarjha83-oss <princekumarjha83-oss@users.noreply.github.com>
2026-07-29 17:08:46 +0530

    Update frontend API URL to use Render backend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://environment-enx2.onrender.com/api' });
 
 // Attach token
 api.interceptors.request.use(cfg => {
